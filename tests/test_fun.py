f1405bfc3e6991b4c490ec51dd2ded9976167dbf
U-GUSTAVLAPTOP\gusta <https://justavgunker@github.com>
2026-01-12 11:47:59 +0100

    unit tests

---
+++ tests/test_fun.py
@@ -0,0 +1,9 @@
+from tests import _PATH_DATA
+
+def adder(a: float, b: float) -> float:
+    return a + b
+
+def test_adder() -> None:
+    assert adder(2, 3) == 5
+    assert adder(-1, 1) == 0
+    assert adder(0, 0) == 0
