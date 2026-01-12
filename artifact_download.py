d6ba3253b7a63c12258c7976eb4a2504ab4f6316
U-GUSTAVLAPTOP\gusta <https://justavgunker@github.com>
2026-01-12 11:16:31 +0100

    initial commit

---
+++ artifact_download.py
@@ -0,0 +1,12 @@
+import wandb
+from src.package.model import MyAwesomeModel
+import torch
+api = wandb.Api()
+artifact_name = f"justav-danmarks-tekniske-universitet-dtu/corrupt_mnist/corrupt_mnist_model:v0"
+artifact = api.artifact(name = artifact_name)
+artifact_dir = artifact.download("artifacts")
+model = MyAwesomeModel()
+model.load_state_dict(torch.load("artifacts/model.pth"))
+
+print("Model loaded from artifact successfully.")
+print(model)
