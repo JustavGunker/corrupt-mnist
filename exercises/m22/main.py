75f0da3ad6b4efa6d19510d4fdc578a3ea92f166
U-GUSTAVLAPTOP\gusta <https://justavgunker@github.com>
2026-01-14 15:06:19 +0100

    exercise m22

---
+++ exercises/m22/main.py
@@ -0,0 +1,82 @@
+# import re
+from fastapi import FastAPI, UploadFile, File
+app = FastAPI()
+from http import HTTPStatus
+
+@app.get("/")
+def root():
+    """ Health check."""
+    response = {
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+    }
+    return response
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
+
+# from enum import Enum
+# class ItemEnum(Enum):
+#     alexnet = "alexnet"
+#     resnet = "resnet"
+#     lenet = "lenet"
+
+# @app.get("/restric_items/{item_id}")
+# def read_item(item_id: ItemEnum):
+#     return {"item_id": item_id}
+
+# @app.get("/query_items")
+# def read_item(item_id: int):
+#     return {"item_id": item_id}
+
+
+# database = {'username': [ ], 'password': [ ]}
+
+# @app.post("/login/")
+# def login(username: str, password: str):
+#     username_db = database['username']
+#     password_db = database['password']
+#     if username not in username_db and password not in password_db:
+#         with open('database.csv', "a") as file:
+#             file.write(f"{username}, {password} \n")
+#         username_db.append(username)
+#         password_db.append(password)
+#     return "login saved"
+
+# @app.get("/text_model/")
+# def contains_email(data: str):
+#     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+#     response = {
+#         "input": data,
+#         "message": HTTPStatus.OK.phrase,
+#         "status-code": HTTPStatus.OK,
+#         "is_email": re.fullmatch(regex, data) is not None
+#     }
+#     return response
+
+# from typing import Optional
+# import cv2
+# from fastapi.responses import FileResponse
+# import anyio
+
+
+# @app.post("/cv_model/")
+# async def cv_model(data: UploadFile = File(...), h: None | int = 28, w: None | int = 28):
+#     """Simple function using open-cv to resize an image."""
+#     async with await anyio.open_file("image.jpg", "wb") as image:
+#         content = await data.read()
+#         image.write(content)
+#         image.close()
+
+#     img = cv2.imread("image.jpg")
+#     res = cv2.resize(img, (h, w))
+
+#     cv2.imwrite("image_resize.jpg", res)
+
+#     return {
+#         "input": data,
+#         "output": FileResponse("image_resize.jpg"),
+#         "message": HTTPStatus.OK.phrase,
+#         "status-code": HTTPStatus.OK,
+#     }
